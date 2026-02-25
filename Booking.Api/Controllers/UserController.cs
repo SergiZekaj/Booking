312c8475eb6fe36ebe8a1143317e438901695860
SergiZekaj <zekajserki17@icloud.com>
2026-02-25 14:44:32 +0100

    Added Register Api and started the structure of Login Api

--- Booking.Api/Controllers/UserController.cs
+++ Booking.Api/Controllers/UserController.cs
@@ -1,6 +1,32 @@
-﻿namespace Booking.Api.Controllers
+﻿using Booking.Application.Features.Users.Register;
+using Booking.Domain.Users;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.Api.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<Guid>> Register([FromBody] CreateUserDto userDto)
+        {
+            var command = new RegisterUserCommand
+            {
+                UserDto = userDto
+            };
+
+            var userId = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(Register), new { id = userId }, userId);
+        }
     }
 }
--- Booking.Api/Program.cs
+++ Booking.Api/Program.cs
@@ -1,15 +1,14 @@
-using Booking.Infrastructure;
-using Booking.Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
+using Booking.Application.Features.DependencyInjection;
+using Booking.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddDbContext<BookingDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+builder.Services.AddControllers();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration.GetConnectionString("DefaultConnection")!);
 
 var app = builder.Build();
 
@@ -21,30 +20,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("v1/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
