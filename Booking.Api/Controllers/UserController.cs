985a591b3249cd16e164db1d56021e9110941be0
SergiZekaj <zekajserki17@icloud.com>
2026-03-10 20:07:15 +0100

    Created and tested UpdateUser&DeleteUser also created the Change/Forgot/ResetPasssword that are to be implemented.

--- Booking.Api/Controllers/UserController.cs
+++ Booking.Api/Controllers/UserController.cs
@@ -1,11 +1,14 @@
 using Booking.Application.Abstractions.Contracts;
-using Booking.Application.Features.Users.Login;
-using Booking.Application.Features.Users.Register;
+using Booking.Application.Features.Users.Commands.Register;
 using Booking.Application.Features.Users;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Booking.Application.Features.Users.Queries.Login;
+using Booking.Application.Features.Users.Queries.GetMyProfile;
+using Booking.Application.Features.Users.Commands.Update;
+using Booking.Application.Features.Users.Commands.Delete;
 
-namespace Booking.Api.Controllers
+namespace Booking.Api.Controllers     // TODO: Admin endpoints - GetAllUsers, ApproveProperty, ManageBookings, ChangeEmail, ReactivateAccount
 {
     [ApiController]
     [Route("api/v1/[controller]")]
@@ -40,6 +43,31 @@ namespace Booking.Api.Controllers
                 return Unauthorized();
 
             return Ok(result);
+        }
+
+        [HttpGet("my-profile")]
+        public async Task<ActionResult<GetMyProfileDto>> GetMyProfile()
+        {
+            var result = await _mediator.Send(new GetMyProfileQuery());
+            return Ok(result);
+        }
+
+        [HttpPut("update")]
+        public async Task<ActionResult<GetMyProfileDto>> Update([FromBody] UpdateUserDto updateUserDto) 
+        {
+            var command = new UpdateUserCommand
+            {
+                Update = updateUserDto
+            };
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<ActionResult> Delete() 
+        {
+            await _mediator.Send(new DeleteUserCommand());
+            return NoContent();
         }
     }
 }
